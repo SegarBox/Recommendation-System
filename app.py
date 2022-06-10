9f8f4c7dcb31edaed80f236a551543032e2dcc25
Bejjoeqq <aldhiya.rozak@gmail.com>
2022-06-11 01:01:10 +0700

    add env

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello_geek():
     return '<h1>Segarbox v1.0.0</h1>'
 
+@app.route('/api/recommendation')
+def recommendation():
+    user_id = request.args.get('user_id')
+    result = App.get.getRecommend(user_id)
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
